b385b50f364549b4ce91c8dc0fecb7276c4bf3d4
Torie Pham <tientp@uci.edu>
2018-10-03 12:26:38 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -9,4 +9,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();//makes app run faster by caching bundle file and save files to local computer to be loaded on
+//however if you update your app, they won't see the change when they first come back to the site
