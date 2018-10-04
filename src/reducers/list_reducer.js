5f5a19d566e2cbbe919b92585e1ff0bca084df14
Torie Pham <tientp@uci.edu>
2018-10-03 17:09:12 -0700

    completed list from server

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -7,6 +7,9 @@ const DEFAULT_STATE = {
 
 export default (state=DEFAULT_STATE,action)=>{
     switch(action.type){
+        case types.GET_LIST_DATA:
+            console.log('Get Data action :', action);
+            return {...state, all: action.payload.data.todos}
         default:
             return state;
     }
