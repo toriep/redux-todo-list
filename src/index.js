b0da0ab3533ac901d6a9c1e786ebe518ae8a5c00
Torie Pham <tientp@uci.edu>
2018-10-05 16:08:14 -0700

    added promise middleware

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
 import rootReducer from './reducers';
 import App from './components/App';
+import rPromise from './middleware/r_promise'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, {}, applyMiddleware(reduxPromise));
+const store = createStore(rootReducer, {}, applyMiddleware(rPromise));
 
 ReactDOM.render(
     <Provider store={store}>
